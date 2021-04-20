de6cba589defdfe724025cc522ad9977095063e5
Naimul Alam <naimul2707@gmail.com>
2021-04-20 07:01:19 +0200

    few tweeks

--- src/App.js
+++ src/App.js
@@ -26,6 +26,9 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
             <PrivateRoute path="/admin">
               <Admin />
             </PrivateRoute>
